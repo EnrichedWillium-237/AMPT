ebfb1ba40b1c14f299b23ba4a31b93f76e939dc8
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-01-26 11:56:24 -0600

    Update

---
+++ test/batch2/qw_ampt_v1_00.py
@@ -0,0 +1,114 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("AMPT")
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100000))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+
+#from Configuration.AlCa.GlobalTag import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:startup', '')
+
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+
+# process.source = cms.Source("PoolSource",
+# 		fileNames = cms.untracked.vstring('file:output.root','),
+# 		secondaryFileNames = cms.untracked.vstring()
+# 		)
+# process.source = cms.Source("PoolSource",
+# 		fileNames = cms.untracked.vstring('/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_1.root'),
+# 		)
+process.source = cms.Source("PoolSource",
+        fileNames = cms.untracked.vstring( *(
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_1.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_10.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_100.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_101.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_102.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_103.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_104.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_106.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_107.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_108.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_109.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_11.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_110.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_111.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_112.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_113.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_114.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_115.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_116.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_117.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_118.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_119.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_12.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_120.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_121.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_122.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_123.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_124.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_125.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_126.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_127.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_128.root',
+                '/store/user/davidlw/AMPT_PbPb5TeV_Gen/mb_string_batch2/160220_111702/0000/amptDefault_cfi_py_GEN_129.root'
+                ) )
+        )
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = cms.untracked.vstring('keep *'),
+    fileName = cms.untracked.string('ampt.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('ana')
+    )
+)
+
+
+process.QWEvent = cms.EDProducer("QWGenEventProducer",
+		trackSrc  = cms.untracked.InputTag("genParticles"),
+		isPrompt  = cms.untracked.bool(False),
+		Etamin = cms.untracked.double(-5.0),
+		Etamax = cms.untracked.double(5.0),
+		ptMin = cms.untracked.double(0.3),
+		ptMax = cms.untracked.double(12.0),
+		)
+
+process.QWHepMC = cms.EDProducer('QWHepMCProducer',
+		src = cms.untracked.InputTag('generator')
+		)
+
+process.QWTreeMaker = cms.EDAnalyzer('QWTreeMaker',
+		src = cms.untracked.InputTag('QWEvent'),
+		vTag = cms.untracked.vstring('phi', 'eta', 'pt', 'charge')
+		)
+
+process.QWHepMCMaker = cms.EDAnalyzer('QWDTreeMaker',
+		src = cms.untracked.InputTag('QWHepMC'),
+		vTag = cms.untracked.vstring('b', 'EP', 'Npart', 'Ncoll')
+		)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('AMPTsample_00.root')
+)
+
+process.ana = cms.Path(process.GeneInfo * process.QWEvent * process.QWTreeMaker * process.QWHepMC * process.QWHepMCMaker)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+process.schedule = cms.Schedule(
+    process.ana,
+#    process.RAWSIMoutput_step,
+)
