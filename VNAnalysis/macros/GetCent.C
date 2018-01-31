28cb7848240959b20c8a9c6eccbf628b214581ee
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-01-31 08:58:13 -0600

    Update

--- VNAnalysis/macros/GetCent.C
+++ VNAnalysis/macros/GetCent.C
@@ -18,9 +18,11 @@
 
 using namespace std;
 
-static const int ncentbins = 13;
+static const int ncentbins = 14;
 static const int cminCENT[] = {0,  5, 10, 15, 20, 25, 30, 35, 40, 50, 60,  0, 20,  60};
 static const int cmaxCENT[] = {5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 20, 60, 100};
+static const int NCbins = 14;
+static const double centbins[] = {0, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100};
 static const int nptbins = 18;
 static const double ptbins[] = {0.30,  0.40,  0.50,  0.60,  0.80,  1.00,  1.25,  1.50,  2.00,  2.50,  3.00,
     3.50,  4.00,  5.00,  6.00,  7.00,  8.00,  10.00,  12.00};
@@ -38,11 +40,10 @@ double Ncoll;
 
 //-- MC inputs
 TH1D * bIN;
-TH1D * epIN;
 TH1D * npartIN;
 TH1D * ncollIN;
 
-TH1D * centbins;
+TH1D * hcent;
 
 TFile * fin;
 TFile * fout;
@@ -67,7 +68,6 @@ void GetCent()
     TH2::SetDefaultSumw2();
 
     bIN     = new TH1D("bIN",     "", 200, 0, 24);
-    epIN    = new TH1D("epIN",    "", 100, -3.5, 3.5);
     npartIN = new TH1D("npartIN", "", 100, 0, 450);
     ncollIN = new TH1D("ncollIN", "", 100, 0, 2000);
     bIN->SetXTitle("b (fm)");
@@ -81,7 +81,6 @@ void GetCent()
     tree0->AddFriend(tree1);
 
     tree0->SetBranchAddress("b",      &b);
-    tree0->SetBranchAddress("EP",     &PsiRP);
     tree0->SetBranchAddress("Npart",  &Npart);
     tree0->SetBranchAddress("Ncoll",  &Ncoll);
 
@@ -107,15 +106,13 @@ void GetCent()
             cout << "\tEstimated total run time: " << Form("%3.2f",elapse1*20./60./60.) << " hours" << endl;
         }
         bIN->Fill( b );
-        epIN->Fill( bounds(1, PsiRP) );
         npartIN->Fill( Npart );
         ncollIN->Fill( Ncoll );
-
     }
 
     // calculate percent centrality from impact parameter
 
-    double bminCent[ncentbins] = {0};
+    double bminCent[NCbins] = {0};
     double totInt = bIN->Integral(0,200);
     double bmin = 0;
     double bmax = 0.12;
@@ -124,27 +121,27 @@ void GetCent()
         if (bmax>=24) continue;
         double binlow = bIN->FindBin(bmin+0.001);
         double binhigh = bIN->FindBin(bmax-0.001);
-        double percntInt = 0.01*(double)(cmaxCENT[cbin]-cminCENT[cbin])*totInt;
+        double percntInt = 0.01*(double)(centbins[cbin+1]-centbins[cbin])*totInt;
         double subInt = bIN->Integral(binlow,binhigh);
         double subPerInt = 100*subInt/totInt;
-        //cout<<"b: "<<bmin<<" - "<<bmax<<"\tbinlow: "<<binlow<<"\tbinhigh: "<<binhigh<<"\tsubInt: "<<subInt<<"\tsubPerInt: "<<subPerInt<<"\tpercntInt: "<<percntInt<<"\tcent: "<<cbin<<"\t"<<cminCENT[cbin]<<" - "<<cmaxCENT[cbin]<<endl;
-        if (subPerInt>((double)cmaxCENT[cbin]-cminCENT[cbin])) {
+        //cout<<"b: "<<bmin<<" - "<<bmax<<"\tbinlow: "<<binlow<<"\tbinhigh: "<<binhigh<<"\tsubInt: "<<subInt<<"\tsubPerInt: "<<subPerInt<<"\tpercntInt: "<<percntInt<<"\tcent: "<<centbins[cbin]<<" - "<<centbins[cbin+1]<<endl;
+        if (subPerInt>((double)centbins[cbin+1]-centbins[cbin])) {
             bminCent[cbin] = bmin;
             bmin = bmax+0.001;
             cbin++;
         }
         bmax+=0.12-0.001;
     }
-    centbins = new TH1D("centbins", "centbins", ncentbins, 0, 120);
-    centbins->SetYTitle("Minimum b (fm)");
-    const char * cname[ncentbins] = {"0_5%", "5_10%", "10_15%", "15_20%", "20_25%", "25_30%", "30_35%", "35_40%", "40_50%", "50_60%", "60_70%", "0_20%", "20_60%"};
+    hcent = new TH1D("centbins", "centbins", ncentbins, centbins);
     for (int i = 0; i<ncentbins; i++) {
-        centbins->Fill(cname[i],bminCent[i]);
-        centbins->SetBinError(i+1,0.12);
+        hcent->SetBinContent(i+1, bminCent[i]);
+        hcent->SetBinError(i+1, 0.12);
     }
-    centbins->SetTitle("");
-    centbins->SetStats(kFALSE);
-    centbins->LabelsDeflate();
+    hcent->SetTitle("");
+    hcent->SetStats(0);
+    hcent->SetYTitle("Minimum b (fm)");
+    hcent->SetXTitle("Centrality (%)");
+    hcent->GetYaxis()->SetRangeUser(0, 18);
 
 
     TCanvas * c0 = new TCanvas("c0","c0",600,550);
@@ -153,7 +150,7 @@ void GetCent()
 
     TCanvas * c1 = new TCanvas("c1","c1",600,550);
     c1->cd();
-    centbins->Draw();
+    hcent->Draw();
 
 
     if (!fopen("hists","r")) system("mkdir hists");
@@ -161,7 +158,7 @@ void GetCent()
     fout = new TFile("hists/Cent.root","recreate");
     fout->cd();
     bIN->Write();
-    centbins->Write();
+    hcent->Write();
     fout->Close();
 
 }
